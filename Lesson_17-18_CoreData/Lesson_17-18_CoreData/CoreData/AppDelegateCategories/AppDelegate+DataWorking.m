e4f1e73b6e915c4f2c8c3fa24e49caa9e3636da5
Andrey Proskurin <andrewproskurin@gmail.com>
2017-10-15 20:45:16 +0300

    Lesson_18. Working with data (NSFetchRequest, NSPredicate, NSSortDescriptor, deleting objects from context...)

---
+++ Lesson_17-18_CoreData/Lesson_17-18_CoreData/CoreData/AppDelegateCategories/AppDelegate+DataWorking.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+DataWorking.m
+//  Lesson_17-18_CoreData
+//
+//  Created by Andrey Proskurin on 13.10.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import "AppDelegate+DataWorking.h"
+
+@implementation AppDelegate (DataWorking)
+
+@end
